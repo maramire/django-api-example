c1a78ece5e2011a0e12cbf6a70b6f78547025c6e
maramire <matias.ramirezg@sansano.usm.cl>
2021-10-01 15:51:21 -0300

    Changes on user serializer, profile can be created in one request

--- socialmedia/serializers.py
+++ socialmedia/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from . import models
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'get_full_name', 'groups']
-
-
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
@@ -47,11 +41,56 @@ class FollowingSerializer(serializers.HyperlinkedModelSerializer):
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     posts = PostSerializer(many=True, read_only=True)
-    user = UserSerializer(read_only=True)
     get_followers = FollowerSerializer(many=True, read_only=True)
     get_following = FollowingSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Profile
-        fields = ['url', 'user', 'bio', 'is_private', 'pic', 'get_followers',
+        fields = ['url', 'bio', 'is_private', 'pic', 'get_followers',
                   'get_following', 'posts']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = ProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'first_name', 'last_name',
+                  'email', 'groups', 'profile']
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        groups_data = validated_data.pop('groups')
+        print(validated_data)
+        user = User(**validated_data)
+        user.save()
+
+        for group in groups_data:
+            user.groups.add(group)
+
+        models.Profile.objects.create(user=user, **profile_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
+        instance.save()
+
+        groups_data = validated_data.pop('groups')
+        for group in groups_data:
+            instance.groups.add(group)
+
+        if (not instance.is_staff):
+            profile = instance.profile
+            profile_data = validated_data.pop('profile')
+            profile.bio = profile_data.get('email', profile.bio)
+            profile.is_private = profile_data.get(
+                'is_private', profile.is_private)
+            profile.pic = profile_data.get('pic', profile.pic)
+            profile.save()
+
+        return instance
